b72c65c168b0df4cd21cd323d9e7625b97d1217c
Giorgi Chapidze <gchapidze741@gmail.com>
2024-06-13 13:20:59 +0400

    Clear up HomePage

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -1,101 +1,155 @@
 import styles from "../css/HomePage.module.css";
 
 import { Outlet, useLocation } from "react-router-dom";
+import { useRef, useState } from "react";
+
 import Index from "./index/Index";
 
 function HomePage() {
   const url = useLocation();
+
+  const [count, setCount] = useState(1);
+
+  const serviceOneRef = useRef(
+    <div className={styles.service_card}>
+      <img
+        src="https://files.elfsightcdn.com/eafe4a4d-3436-495d-b748-5bdce62d911d/99334b94-5373-4aec-99bb-e027c93db2e2/saitistvis.png"
+        alt=""
+      />
+      <h2>
+        ვაკის და საბურთალოს ფილიალი
+        <span> ნინო რამიშვილის 20 - სანდრო ეულის 7 </span>
+      </h2>
+      <ul>
+        <li>კარდიო ზონა</li>
+        <li>მძიმე წონების ზონა</li>
+        <li>გარე სავარჯიშო სივრცე</li>
+        <li>Techno Gym ტრენაჟორები</li>
+        <li>წევრთა ლაუნჯი</li>
+        <li>მასაჟის სივრცე</li>
+      </ul>
+      <div className={styles.prices}>
+        <h3>
+          <span>₾ 205</span> / 1 თვე
+        </h3>
+        <h4>₾185 / 3 თვე (₾555)</h4>
+        <h5>₾165/ 6 თვე (₾990)</h5>
+        <h6>₾150 / 12 თვე (₾1,805)</h6>
+      </div>
+    </div>
+  );
+
+  const serviceTwoRef = useRef(
+    <div className={styles.service_card}>
+      <img
+        src="https://files.elfsightcdn.com/eafe4a4d-3436-495d-b748-5bdce62d911d/a4961302-aa84-4fc2-b108-1bd56f4e47e3/1-saitistvis.png"
+        alt=""
+      />
+      <h2>
+        სითი მოლის და მზიურის ფილიალი
+        <span>პეტრე ქავთარაძე 1 (სითი მოლი) - ჭაბუა ამირეჯიბის 2</span>
+      </h2>
+      <ul>
+        <li>კარდიო ზონა</li>
+        <li>მძიმე წონების ზონა</li>
+        <li>Life Fitness ტრენაჟორები</li>
+        <li>ჯგუფური ვარჯიშების სტუდია</li>
+        <li>ფუნქციონალური ვარჯიშების ზონა</li>
+        <li>Ergoline სოლარიუმი</li>
+      </ul>
+      <div className={styles.prices}>
+        <h3>
+          <span>₾ 285</span> / 1 თვე
+        </h3>
+        <h4>₾245 / 3 თვე (₾555)</h4>
+        <h5>₾225/ 6 თვე (₾990)</h5>
+        <h6>₾195 / 12 თვე (₾1,805)</h6>
+      </div>
+    </div>
+  );
+
+  const serviceThreeRef = useRef(
+    <div className={styles.service_card}>
+      <img
+        src="https://files.elfsight.com/storage/1d330207-1ad5-4d39-bb68-27190fd2e199/62dc4a50-4b2a-4700-a676-12415b7e0e52.jpeg"
+        alt=""
+      />
+      <h2>
+        ბათუმის ფილიალი
+        <span> შერიფ ხიმსიაშვილის 29 (გრანდ მოლი) </span>
+      </h2>
+      <ul>
+        <li>კარდიო ზონა</li>
+        <li>მძიმე წონების ზონა</li>
+        <li>Techno Gym ტრენაჟორები</li>
+        <li>მასაჟის სივრცე</li>
+        <li>დახურული პარკინგი</li>
+      </ul>
+      <div className={styles.prices}>
+        <h3>
+          <span>₾ 205</span> / 1 თვე
+        </h3>
+        <h4>₾185 / 3 თვე (₾555)</h4>
+        <h5>₾165/ 6 თვე (₾990)</h5>
+        <h6>₾150 / 12 თვე (₾1,805)</h6>
+      </div>
+    </div>
+  );
 
   return (
     <>
       <Index />
       {url.pathname === "/" ? (
         <>
-          <main className={styles.main}></main>
-          <div className={styles.services}>
-            <div className={styles.service_card}>
-              <img
-                src="https://files.elfsightcdn.com/eafe4a4d-3436-495d-b748-5bdce62d911d/99334b94-5373-4aec-99bb-e027c93db2e2/saitistvis.png"
-                alt=""
-              />
-              <h2>
-                ვაკის და საბურთალოს ფილიალი
-                <span> ნინო რამიშვილის 20 - სანდრო ეულის 7 </span>
-              </h2>
-              <ul>
-                <li>კარდიო ზონა</li>
-                <li>მძიმე წონების ზონა</li>
-                <li>გარე სავარჯიშო სივრცე</li>
-                <li>Techno Gym ტრენაჟორები</li>
-                <li>წევრთა ლაუნჯი</li>
-                <li>მასაჟის სივრცე</li>
-              </ul>
-              <div className={styles.prices}>
-                <h3>
-                  <span>₾ 205</span> / 1 თვე
-                </h3>
-                <h4>₾185 / 3 თვე (₾555)</h4>
-                <h5>₾165/ 6 თვე (₾990)</h5>
-                <h6>₾150 / 12 თვე (₾1,805)</h6>
-              </div>
+          <main className={styles.main}>
+            <div className={styles.services}>
+              {screen.width <= 600 && (
+                <>
+                  <img
+                    src="/left-arrow.svg"
+                    width="30px"
+                    style={{
+                      padding: "5px",
+                      cursor: "pointer",
+                      borderRadius: "40px",
+                      backgroundColor: "green",
+                      position: "absolute",
+                      left: "2%",
+                    }}
+                    onClick={() =>
+                      setCount((count) => {
+                        if (count > 1) return count - 1;
+                        else return count;
+                      })
+                    }
+                  />
+                  <img
+                    src="/right-arrow.svg"
+                    width="30px"
+                    style={{
+                      padding: "5px",
+                      cursor: "pointer",
+                      borderRadius: "40px",
+                      backgroundColor: "green",
+                      position: "absolute",
+                      right: "2%",
+                    }}
+                    onClick={() =>
+                      setCount((count) => {
+                        if (count < 3) return count + 1;
+                        else return count;
+                      })
+                    }
+                  />
+                  {count === 1
+                    ? serviceOneRef.current
+                    : count === 2
+                    ? serviceTwoRef.current
+                    : serviceThreeRef.current}
+                </>
+              )}
             </div>
-
-            <div className={styles.service_card}>
-              <img
-                src="https://files.elfsightcdn.com/eafe4a4d-3436-495d-b748-5bdce62d911d/a4961302-aa84-4fc2-b108-1bd56f4e47e3/1-saitistvis.png"
-                alt=""
-              />
-              <h2>
-                სითი მოლის და მზიურის ფილიალი
-                <span>
-                  პეტრე ქავთარაძე 1 (სითი მოლი) - ჭაბუა ამირეჯიბის 2 (მზიური
-                  ბიზნეს ცენტრი){" "}
-                </span>
-              </h2>
-              <ul>
-                <li>კარდიო ზონა</li>
-                <li>მძიმე წონების ზონა</li>
-                <li>Life Fitness ტრენაჟორები</li>
-                <li>ჯგუფური ვარჯიშების სტუდია</li>
-                <li>ფუნქციონალური ვარჯიშების ზონა</li>
-                <li>Ergoline სოლარიუმი</li>
-              </ul>
-              <div className={styles.prices}>
-                <h3>
-                  <span>₾ 285</span> / 1 თვე
-                </h3>
-                <h4>₾245 / 3 თვე (₾555)</h4>
-                <h5>₾225/ 6 თვე (₾990)</h5>
-                <h6>₾195 / 12 თვე (₾1,805)</h6>
-              </div>
-            </div>
-
-            <div className={styles.service_card}>
-              <img
-                src="https://files.elfsight.com/storage/1d330207-1ad5-4d39-bb68-27190fd2e199/62dc4a50-4b2a-4700-a676-12415b7e0e52.jpeg"
-                alt=""
-              />
-              <h2>
-                ბათუმის ფილიალი
-                <span> შერიფ ხიმსიაშვილის 29 (გრანდ მოლი) </span>
-              </h2>
-              <ul>
-                <li>კარდიო ზონა</li>
-                <li>მძიმე წონების ზონა</li>
-                <li>Techno Gym ტრენაჟორები</li>
-                <li>მასაჟის სივრცე</li>
-                <li>დახურული პარკინგი</li>
-              </ul>
-              <div className={styles.prices}>
-                <h3>
-                  <span>₾ 205</span> / 1 თვე
-                </h3>
-                <h4>₾185 / 3 თვე (₾555)</h4>
-                <h5>₾165/ 6 თვე (₾990)</h5>
-                <h6>₾150 / 12 თვე (₾1,805)</h6>
-              </div>
-            </div>
-          </div>
+          </main>
         </>
       ) : (
         <Outlet />
